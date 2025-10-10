645d7fee0a9217cd391b1a120fcecd0a7f21ccc7
NicaZadirieva <no.demchenko@yandex.ru>
2025-10-10 16:17:20 +0300

    feat: добавила счетчик

--- src/pages/home/ui/HomePage/HomePage.jsx
+++ src/pages/home/ui/HomePage/HomePage.jsx
@@ -8,6 +8,10 @@ export function HomePage() {
             <Button>Посмотреть</Button>
             <Button appearance="coloured">Посмотреть</Button>
             <Button appearance="shadowed">Посмотреть</Button>
+            Со счетчиком
+            <Button counter={10}>Посмотреть</Button>
+            <Button counter={10} appearance="coloured">Посмотреть</Button>
+            <Button counter={10} appearance="shadowed">Посмотреть</Button>
         </div>
     );
 }
--- src/shared/ui/Button/Button.jsx
+++ src/shared/ui/Button/Button.jsx
@@ -1,12 +1,15 @@
 import cn from 'classnames';
 import styles from './Button.module.css';
 
-export function Button({children, className, appearance = 'outlined', icon, ...props}) {
+export function Button({children, className, appearance = 'outlined', icon, counter, ...props}) {
+    const Counter = <span className={styles.counter}>{counter}</span>;
+
     return <button className={cn(className, styles.button, {
         [styles.outlined]: appearance == "outlined",
         [styles.shadowed]: appearance == "shadowed",
         [styles.coloured]: appearance == "coloured"
     })} {...props}>
         {children}
+        {counter && Counter}
         </button>
 }
