70bd889568b296017f804060e72d0f196f5fa180
NicaZadirieva <no.demchenko@yandex.ru>
2025-10-10 16:47:20 +0300

    feat: выделила все иконки

---
+++ src/assets/Icon/index.js
@@ -0,0 +1,39 @@
+import Confetti from "./icons/Confetti.svg";
+import StatisticsBonuses from "./icons/statistics/bonuses.svg";
+import Skills from "./icons/statistics/skills.svg";
+import StudyTime from "./icons/statistics/studyTime.svg";
+import DoneLessons from "./icons/statistics/doneLessons.svg";
+import Achievements from "./icons/statistics/achievements.svg";
+import Search from "./icons/search.svg";
+import Updates from "./icons/updates.svg";
+import ProcessedTime from "./icons/cards/processedTime.svg";
+import IncludeLessons from "./icons/cards/includeLessons.svg";
+import Star from "./icons/star.svg";
+import Courses from "./icons/panel/courses.svg";
+import Map from "./icons/panel/map.svg";
+import PurpleSchool from "./icons/panel/purpleplus.svg";
+import Community from "./icons/panel/community.svg";
+import Bonuses from "./icons/panel/bonuses.svg";
+import Profile from "./icons/panel/profile.svg";
+import Help from "./icons/panel/help.svg";
+
+export const Icon = {
+    Achievements,
+    Bonuses,
+    Community,
+    Confetti,
+    Courses,
+    DoneLessons,
+    Map,
+    ProcessedTime,
+    Profile,
+    PurpleSchool,
+    Search,
+    Skills,
+    Star,
+    StatisticsBonuses,
+    StudyTime,
+    Updates,
+    Help,
+    IncludeLessons
+}
