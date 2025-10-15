68751ec4056fabd2b6a71afd0e2e8abccbdc1b34
NicaZadirieva <no.demchenko@yandex.ru>
2025-10-15 21:39:04 +0300

    feat: добавлены компоненты без реализации

--- src/shared/ui/index.js
+++ src/shared/ui/index.js
@@ -5,5 +5,9 @@ import { Graph } from "./Graph";
 import { Tag } from "./Tag";
 import { InfoBox } from './InfoBox';
 import { Tabs } from "./Tabs";
+import { Checkbox } from "./Checkbox";
+import { RadioButton } from "./RadioButton";
+import { Link } from './Link';
+import { Rating } from "./Rating";
 
-export { Button, Card, CloseButton, Graph, Tag, InfoBox, Tabs };
+export { Button, Rating, Card, CloseButton, Graph, Tag, InfoBox, Tabs, Checkbox, RadioButton, Link };
